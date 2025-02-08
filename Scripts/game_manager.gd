21ff0e0855929a40be292e7e3580d61301fee13b
Markuss Sprogis <markuss.sprogis@dataductus.se>
2025-02-08 15:48:58 +0100

    Rim job

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -5,6 +5,7 @@ const TILE_SCALE = 2
 var TICK_IN_SECONDS = 1
 
 const MAP_SIZE = 64
+const RIM_SIZE = 32
 const NUM_START_FIRES = 5
 var tiles_map = []
 var fire_tile_list = []
@@ -16,10 +17,11 @@ var tile_scene = preload("res://Scenes/tile.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	create_map()
+	create_rim()
 	light_initial_fires()
 	TIMER.wait_time = TICK_IN_SECONDS
 	TIMER.start()
-
+	
 func create_map():
 	for z in range(MAP_SIZE):
 		var row = []
@@ -33,7 +35,17 @@ func create_map():
 	for z in range(MAP_SIZE):
 		for x in range(MAP_SIZE):
 			connect_neighbors(tiles_map[z][x])
-			
+
+func create_rim():
+	for z in range(-RIM_SIZE, MAP_SIZE+RIM_SIZE):
+		for x in range(-RIM_SIZE, MAP_SIZE+RIM_SIZE):
+			if (x >= 0 && x < MAP_SIZE) && (z >= 0 && z < MAP_SIZE):
+				continue
+			var rim_tile = tile_scene.instantiate()
+			rim_tile.state = 2
+			rim_tile.pos = Vector3(x * TILE_SCALE, 0, z * TILE_SCALE)
+			add_child(rim_tile)
+
 func connect_neighbors(tile):
 	var x = tile.pos.x / TILE_SCALE
 	var z = tile.pos.z / TILE_SCALE
