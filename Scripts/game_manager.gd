c325eb4b5d3560a0db538ce68cf11d63a5a79a39
Jakob Andersson <jakob@plick.se>
2025-02-08 11:51:10 +0100

    Add base GameManager

---
+++ Scripts/game_manager.gd
@@ -0,0 +1,35 @@
+extends Node3D
+
+const MAP_SIZE = 10
+var tiles_map = []
+var tile_scene = preload("res://Scenes/tile.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	create_map()
+
+func create_map():
+	for z in range(MAP_SIZE):
+		var row = []
+		for x in range(MAP_SIZE):
+			var tile = tile_scene.instantiate()
+			tile.pos = Vector3(x, 0, z)
+			add_child(tile)
+			row.append(tile)
+		tiles_map.append(row)
+		
+	for z in range(MAP_SIZE):
+		for x in range(MAP_SIZE):
+			connect_neighbors(tiles_map[z][x])
+			
+func connect_neighbors(tile):
+	var x = tile.pos.x
+	var z = tile.pos.z
+	if x > 0: # Has west neighbor
+		tile.n_west = tiles_map[z][x-1]
+	if x < (MAP_SIZE - 1): # Has east neighbor
+		tile.n_west = tiles_map[z][x+1]
+	if z > 0: # Has north neighbor
+		tile.n_west = tiles_map[z-1][x]
+	if z < (MAP_SIZE - 1): # Has south neighbor
+		tile.n_west = tiles_map[z+1][x]
--- Scripts/tile.gd
+++ Scripts/tile.gd
@@ -30,6 +30,7 @@ func render():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	mesh = $StaticBody/Mesh
+	position = pos
 	render()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
