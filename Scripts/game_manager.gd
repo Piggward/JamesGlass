48adc6a273ed6bdfad57e9895190f7361658d0e1
Jakob Andersson <jakob@plick.se>
2025-02-08 13:12:13 +0100

    Add light shit on fire

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -2,15 +2,23 @@ extends Node3D
 
 const TILE_SCALE = 2
 
-const MAP_SIZE = 10
+var TICK_IN_SECONDS = 1
+
+const MAP_SIZE = 64
 const NUM_START_FIRES = 5
 var tiles_map = []
+var fire_tile_list = []
+var dry_tile_list = []
 var tile_scene = preload("res://Scenes/tile.tscn")
+
+@onready var TIMER = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	create_map()
 	light_initial_fires()
+	TIMER.wait_time = TICK_IN_SECONDS
+	TIMER.start()
 
 func create_map():
 	for z in range(MAP_SIZE):
@@ -30,13 +38,13 @@ func connect_neighbors(tile):
 	var x = tile.pos.x / TILE_SCALE
 	var z = tile.pos.z / TILE_SCALE
 	if x > 0: # Has west neighbor
-		tile.n_west = tiles_map[z][x-1]
+		tile.neighbours[0] = tiles_map[z][x-1]
 	if x < (MAP_SIZE - 1): # Has east neighbor
-		tile.n_west = tiles_map[z][x+1]
+		tile.neighbours[2] = tiles_map[z][x+1]
 	if z > 0: # Has north neighbor
-		tile.n_west = tiles_map[z-1][x]
+		tile.neighbours[1] = tiles_map[z-1][x]
 	if z < (MAP_SIZE - 1): # Has south neighbor
-		tile.n_west = tiles_map[z+1][x]
+		tile.neighbours[3] = tiles_map[z+1][x]
 
 func light_initial_fires():
 	for i in range(NUM_START_FIRES):
@@ -44,6 +52,16 @@ func light_initial_fires():
 		var z = randi_range(0, MAP_SIZE-1)
 		
 		var tile = tiles_map[z][x]
-		tile.light_fire()
+		fire_tile_list.append(tile)
+		dry_tile_list.append_array(tile.light_fire())
+		
+func light_shit_on_fire():
+	var new_dry_tile_list = []
+	for dry_tile in dry_tile_list:
+		new_dry_tile_list.append_array(dry_tile.light_fire())
+		fire_tile_list.append(dry_tile)
+	dry_tile_list = new_dry_tile_list
 		
 	
+func _on_timer_timeout():
+	light_shit_on_fire()
