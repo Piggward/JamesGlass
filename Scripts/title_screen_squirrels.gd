dce9ee064634d27d61d0c4a7670d7ab79bee8837
Gustav Larsson <nitrp8@gmail.com>
2025-02-09 16:53:50 +0100

    lagt till titel scene med ekorrar

---
+++ Scripts/title_screen_squirrels.gd
@@ -0,0 +1,18 @@
+extends Node3D
+@onready var animation_player1: AnimationPlayer = $squirrel/AnimationPlayer
+@onready var animation_player2: AnimationPlayer = $squirrel2/AnimationPlayer
+@onready var animation_boat: AnimationPlayer = $AnimationPlayer
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#animation_player1.play("Squirrel 1 | Menu_screen_1")
+	#animation_player2.play("Squirrel 1 | Menu_screen_2")
+	animation_boat.play("titlescreen_boat_bob")
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
